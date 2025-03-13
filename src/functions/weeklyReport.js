4a4a7e41bcb0e165ee0ed9f4eb3d383f39a9047a
chetana.bangre <chetana.bangre@goavega.com>
2025-03-13 10:32:49 +0530

    added UAT HTTP link

--- src/functions/weeklyReport.js
+++ src/functions/weeklyReport.js
@@ -5,7 +5,7 @@ app.timer('weeklyReport', {
     handler: async (myTimer, context) => {
         context.log(`Weekly report function executed at: ${new Date().toISOString()}`);
         try {
-            const response = await fetch("http://127.0.0.1:8000/trigger-report", {
+            const response = await fetch("https://landalert-uat-appsvc.azurewebsites.net/trigger-report", {
                 method: "GET",
                 headers: { "Content-Type": "application/json" }
             });
